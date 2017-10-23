08403dc1866262d7ff12b9a342a95ad96644035c
IsNology <glenn@isnology.com>
2017-10-24 10:42:17 +1100

    add stock prices seeded from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+msft_prices = HTTParty.get('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=MSFT&apikey=demo')
+
+msft_prices['Weekly Time Series'].each do |date_string, prices|
+  StockPrice.create!(symbol: 'MSFT', date: date_string, open: prices['1. open'], high: prices['2. high'], low:
+                             prices['3. low'], close: prices['4. close'], volume: prices['5. volume'])
+end
